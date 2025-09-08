919c591eae8eb19abe88ce4f592711c2c7168c96
Marcossousadev <nossogm@gmail.com>
2025-09-08 17:17:59 -0300

    feat: alert cpf 11 characters

--- src/types/schemas/auth/client-schema.ts
+++ src/types/schemas/auth/client-schema.ts
@@ -5,7 +5,7 @@ export const clientSchema = z.object({
     role: z.literal('CLIENT'),
     fullName: z.string().min(9, "No mínimo o nome deve conter 9 caracteres"),
     age: z.number().int().positive(),
-    cpf: z.string().min(14, "O CPF deve conter 14 caracteres"),
+    cpf: z.string().min(11, "O CPF deve conter 14 caracteres"),
     phone: z.string(),
     stay: z.string(),
     city: z.string()
